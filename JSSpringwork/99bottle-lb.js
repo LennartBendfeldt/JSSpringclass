0163c94c8ebe762597dd221b9229947fcb4b56e8
Lennart Bendfeldt <lennartbendfeldt@hotmail.com>
2019-04-04 16:07:48 -0400

    99 bottles

---
+++ JSSpringwork/99bottle-lb.js
@@ -0,0 +1,13 @@
+var bottles = 99;
+
+for(bottles; bottles >= 0; bottles --){
+
+    if(bottles == 0){
+
+        console.log("Get some more beer!");
+    } else {
+
+        console.log(bottles + " of beer on the wall. Take one down, pass it around.");
+        
+    }
+}
