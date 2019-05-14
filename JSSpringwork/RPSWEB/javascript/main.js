d3c7cf0d348e4f5ebff331159d448c9098275103
LennartBendfeldt <46977057+LennartBendfeldt@users.noreply.github.com>
2019-05-14 12:15:15 -0400

    updates to RPS and todolist

--- JSSpringwork/RPSWEB/javascript/main.js
+++ JSSpringwork/RPSWEB/javascript/main.js
@@ -1,4 +1,10 @@
-var hands = ['rock', 'paper', 'scissors'];
+//All possible hands
+
+const hands = ['rock', 'paper', 'scissors'];
+
+//Set win conditions by creating an object with 
+//name:value pairs equal to desired win conditions; declare default values
+//for number of rounds to play to.
 
 const winConditions = {
     rock: "scissors",
@@ -6,35 +12,49 @@ const winConditions = {
     scissors: "paper",
 };
 
-function getHand(){
+var playUntil = 5;
 
-    return hands[Math.floor(Math.random() * hands.length)];
-}
+//Declaring DOM variables for editing of the view
+
+var playbutton1 = document.getElementById('playbutton');
+var playtext1 = document.getElementById('Play');
+var gameinfo = document.getElementById('gameinfo');
+
+//Declare player objects
 
 var player1 = {
 
     name: 'John',
     handp1: getHand(),
     score: 0,
-}
+};
 
 var player2 = {
 
     name: 'Larry',
     handp2: getHand(),
     score: 0,
-}
+};
 
-function playRound(player1, player2){
+//Create random hands out of the options in the hands array
+
+function getHand() {
+
+    return hands[Math.floor(Math.random() * hands.length)];
+};
+
+//Function the plays the round and takes players as parameters
+
+function playRound(player1, player2) {
 
     const player1hand = getHand();
     const player2hand = getHand();
 
-    if(player1hand === player2hand){
+    if (player1hand === player2hand) {
 
         console.log('Its a tie');
 
-    } else if(winConditions[player1hand] === player2hand){
+    } else if (winConditions[player1hand] === player2hand) {
         gameinfo.lastElementChild.firstElementChild.innerHTML = "Player 1 wins!";
         player1.score = player1.score + 1;
         console.log(player1.score, player2.score);
@@ -43,39 +63,38 @@ function playRound(player1, player2){
         gameinfo.lastElementChild.firstElementChild.innerHTML = "Player 2 wins!";
         player2.score = player2.score + 1;
         console.log(player1.score, player2.score);
-    }
-}
-
-var playUntil = 5;
-
-function playGame(player1, player2, playUntil){
-
-
-    while((player1.score < playUntil) && (player2.score < playUntil)){
-
-        
-        playRound(player1, player2);
-        
     };
 };
 
-var playbutton1 = document.getElementById('playbutton');
-var playtext1 = document.getElementById('Play');
-var gameinfo = document.getElementById('gameinfo');
+//Use the players and playUntil as parameters to run a full game to the 
+//specified amount of rounds.
 
-playbutton1.onclick = function rungame(){
+function playGame(player1, player2, playUntil) {
 
-player1.score = 0;
-player2.score = 0;
 
-playbutton1.innerHTML = "Play again?";
-playtext1.remove();
+    while ((player1.score < playUntil) && (player2.score < playUntil)) {
 
-playGame(player1, player2, playUntil);
-gameinfo.firstElementChild.firstElementChild.innerHTML = "Player 1: " + player1.name;
-gameinfo.firstElementChild.firstElementChild.nextElementSibling.innerHTML = "Score: " + player1.score;
-gameinfo.firstElementChild.nextElementSibling.firstElementChild.innerHTML = "Player 2: " + player2.name;
-gameinfo.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.innerHTML = "Score: " + player2.score;
-}
+
+        playRound(player1, player2);
+
+    };
+};
+
+//Javascript to run the game and change the DOM on a click of the play button
+
+playbutton1.onclick = function rungame() {
+
+    player1.score = 0;
+    player2.score = 0;
+
+    playbutton1.innerHTML = "Play again?";
+    playtext1.remove();
+
+    playGame(player1, player2, playUntil);
+    gameinfo.firstElementChild.firstElementChild.innerHTML = "Player 1: " + player1.name;
+    gameinfo.firstElementChild.firstElementChild.nextElementSibling.innerHTML = "Score: " + player1.score;
+    gameinfo.firstElementChild.nextElementSibling.firstElementChild.innerHTML = "Player 2: " + player2.name;
+    gameinfo.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.innerHTML = "Score: " + player2.score;
+};
 
 
---
+++ JSSpringwork/todolist/javascript/main.js
@@ -0,0 +1,20 @@
+var del0101 = document.getElementById('del0101');
+
+del0101.onclick = function deleteAdd(){
+
+    // if(del0101.parentElement.parentElement.parentElement.childElementCount === 1){
+    //     del0101.parentElement.appendChild
+    // }
+    del0101.parentElement.parentElement.remove();
+};
+
+var add0101 = document.getElementById('add0101');
+
+
+add0101.onclick = function addd(){
+    var a = document.getElementById('myText').value;
+    
+}
+document.getElementById('test').innerHTML = a;
+
+
